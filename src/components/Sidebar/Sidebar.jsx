a4e78256fdc9a56a6092229c959c2c000a502552
VasylPopaz <pafos1997@gmail.com>
2025-11-24 16:53:16 +0100

    Implement hamburger menu for Sidebar on mobile

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,4 +1,7 @@
+import { useCallback, useEffect, useState } from "react";
+
 import s from "./Sidebar.module.css";
+
 const CHATS = [
   {
     id: 1,
@@ -22,23 +25,78 @@ const CHATS = [
   },
 ];
 const Sidebar = ({ chats = CHATS, activeChatId = 1 }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleSidebar = useCallback(() => {
+    setIsOpen((prev) => !prev);
+  }, []);
+
+  const handleEscapePress = (event) => {
+    if (isOpen && event.key === "Escape") {
+      setIsOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    const handleEscape = (e) => {
+      if (isOpen && e.code === "Escape") {
+        toggleSidebar();
+      }
+    };
+
+    if (toggleSidebar) {
+      document.addEventListener("keydown", handleEscape);
+    } else {
+      document.removeEventListener("keydown", handleEscape);
+    }
+
+    return () => {
+      document.removeEventListener("keydown", handleEscape);
+    };
+  }, [isOpen, toggleSidebar]);
+
   return (
-    <div className={s.sidebar}>
-      <ul className={s.chats}>
-        {chats.map((chat) => (
-          <li
-            key={chat.id}
-            className={s.chat}
-            data-active={chat.id === activeChatId}
-          >
-            <button type="button" className={s.chatButton}>
-              <div className={s.chatTitle}> {chat.title}</div>
-            </button>
-          </li>
-        ))}
-      </ul>
-    </div>
+    <>
+      <button
+        type="button"
+        className={s.menuButton}
+        onClick={toggleSidebar}
+        onKeyDown={handleEscapePress}
+      >
+        <MenuIcon />
+      </button>
+      <div className={s.sidebar} data-open={isOpen}>
+        <ul className={s.chats}>
+          {chats.map((chat) => (
+            <li
+              key={chat.id}
+              className={s.chat}
+              data-active={chat.id === activeChatId}
+            >
+              <button type="button" className={s.chatButton}>
+                <div className={s.chatTitle}> {chat.title}</div>
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+      {isOpen && <div className={s.overlay} onClick={toggleSidebar}></div>}
+    </>
   );
 };
 
 export default Sidebar;
+
+const MenuIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      height="24px"
+      viewBox="0 -960 960 960"
+      width="24px"
+      fill="#1f1f1f"
+    >
+      <path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z" />
+    </svg>
+  );
+};
